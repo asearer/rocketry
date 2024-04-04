d2208d49f39e6463134bd83db3854b723b66adcd
asearer <amsearer84@gmail.com>
2024-04-04 16:51:57 -0400

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   launch_sim/launch_sim.py Added angle/thrust inputs

--- launch_sim/launch_sim.py
+++ launch_sim/launch_sim.py
@@ -33,12 +33,13 @@ font = pygame.font.Font(None, 36)
 
 # Rocket class
 class Rocket:
-    def __init__(self, x, y, velocity, acceleration, angle):
+    def __init__(self, x, y, velocity, acceleration, angle, thrust):
         self.x = x
         self.y = y
         self.velocity = velocity
         self.acceleration = acceleration
         self.angle = angle
+        self.thrust = thrust
 
     def update(self):
         self.velocity += self.acceleration
@@ -61,14 +62,27 @@ def draw_button(text, font, color, rect):
 def main():
     rocket_velocity = ""
     rocket_acceleration = ""
+    rocket_angle = ""
+    rocket_thrust = ""
 
     input_velocity = ""
     input_acceleration = ""
+    input_angle = ""
+    input_thrust = ""
+
     velocity_active = False
     acceleration_active = False
+    angle_active = False
+    thrust_active = False
 
-    rocket = Rocket(WIDTH // 2, HEIGHT, 0, 0, 90)  # Initial angle set to 90 degrees
+    rocket = Rocket(WIDTH // 2, HEIGHT, 0, 0, 90, 0)  # Initial angle set to 90 degrees
     running = True
+
+    # Define input field rectangles
+    velocity_rect = pygame.Rect(100, 140, 100, 30)
+    acceleration_rect = pygame.Rect(100, 240, 100, 30)
+    angle_rect = pygame.Rect(100, 340, 100, 30)
+    thrust_rect = pygame.Rect(100, 440, 100, 30)
 
     # Define launch button rectangle
     launch_button_rect = pygame.Rect(10, 70, 120, 30)
@@ -82,18 +96,36 @@ def main():
                 if velocity_rect.collidepoint(event.pos):
                     velocity_active = True
                     acceleration_active = False
+                    angle_active = False
+                    thrust_active = False
                 elif acceleration_rect.collidepoint(event.pos):
                     acceleration_active = True
                     velocity_active = False
+                    angle_active = False
+                    thrust_active = False
+                elif angle_rect.collidepoint(event.pos):
+                    angle_active = True
+                    velocity_active = False
+                    acceleration_active = False
+                    thrust_active = False
+                elif thrust_rect.collidepoint(event.pos):
+                    thrust_active = True
+                    velocity_active = False
+                    acceleration_active = False
+                    angle_active = False
                 else:
                     velocity_active = False
                     acceleration_active = False
+                    angle_active = False
+                    thrust_active = False
                 # Check if mouse click is within launch button
                 if launch_button_rect.collidepoint(event.pos):
                     # Launch the rocket with current parameters
                     try:
                         rocket.velocity = float(rocket_velocity)
                         rocket.acceleration = float(rocket_acceleration)
+                        rocket.angle = float(rocket_angle)
+                        rocket.thrust = float(rocket_thrust)
                     except ValueError:
                         pass
             elif event.type == pygame.MOUSEMOTION:
@@ -112,7 +144,7 @@ def main():
                         input_velocity = input_velocity[:-1]
                     else:
                         input_velocity += event.unicode
-                if acceleration_active:
+                elif acceleration_active:
                     if event.key == pygame.K_RETURN:
                         try:
                             rocket_acceleration = input_acceleration
@@ -123,6 +155,28 @@ def main():
                         input_acceleration = input_acceleration[:-1]
                     else:
                         input_acceleration += event.unicode
+                elif angle_active:
+                    if event.key == pygame.K_RETURN:
+                        try:
+                            rocket_angle = input_angle
+                            input_angle = ""
+                        except ValueError:
+                            pass
+                    elif event.key == pygame.K_BACKSPACE:
+                        input_angle = input_angle[:-1]
+                    else:
+                        input_angle += event.unicode
+                elif thrust_active:
+                    if event.key == pygame.K_RETURN:
+                        try:
+                            rocket_thrust = input_thrust
+                            input_thrust = ""
+                        except ValueError:
+                            pass
+                    elif event.key == pygame.K_BACKSPACE:
+                        input_thrust = input_thrust[:-1]
+                    else:
+                        input_thrust += event.unicode
 
         screen.fill(WHITE)
 
@@ -131,18 +185,32 @@ def main():
 
         # Display text and input fields
         draw_text("Rocket Velocity:", font, (0, 0, 0), 10, 110)
-        velocity_rect = pygame.draw.rect(screen, (0, 0, 0), (100, 140, 100, 30), 2)  # Adjusted x-coordinate
         if velocity_active:
             draw_text(input_velocity, font, (0, 0, 0), 105, 150)  # Adjusted x-coordinate
         else:
             draw_text(rocket_velocity, font, (0, 0, 0), 105, 150)  # Adjusted x-coordinate
+        pygame.draw.rect(screen, (0, 0, 0), velocity_rect, 2)
 
         draw_text("Rocket Acceleration:", font, (0, 0, 0), 10, 210)
-        acceleration_rect = pygame.draw.rect(screen, (0, 0, 0), (100, 240, 100, 30), 2)  # Adjusted x-coordinate
         if acceleration_active:
             draw_text(input_acceleration, font, (0, 0, 0), 105, 250)  # Adjusted x-coordinate
         else:
             draw_text(rocket_acceleration, font, (0, 0, 0), 105, 250)  # Adjusted x-coordinate
+        pygame.draw.rect(screen, (0, 0, 0), acceleration_rect, 2)
+
+        draw_text("Rocket Angle:", font, (0, 0, 0), 10, 310)
+        if angle_active:
+            draw_text(input_angle, font, (0, 0, 0), 105, 350)  # Adjusted x-coordinate
+        else:
+            draw_text(rocket_angle, font, (0, 0, 0), 105, 350)  # Adjusted x-coordinate
+        pygame.draw.rect(screen, (0, 0, 0), angle_rect, 2)
+
+        draw_text("Rocket Thrust:", font, (0, 0, 0), 10, 410)
+        if thrust_active:
+            draw_text(input_thrust, font, (0, 0, 0), 105, 450)  # Adjusted x-coordinate
+        else:
+            draw_text(rocket_thrust, font, (0, 0, 0), 105, 450)  # Adjusted x-coordinate
+        pygame.draw.rect(screen, (0, 0, 0), thrust_rect, 2)
 
         # Update rocket
         rotated_rocket = pygame.transform.rotate(rocket_image, rocket.angle)
@@ -157,3 +225,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
